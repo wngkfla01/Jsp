b0144fff6af093d842697cecdbde74a4ece57d49
Halim <wngkfla01@gmail.com>
2020-06-01 12:45:45 +0900

    JSP Ch02

--- Ch02/src/sub1/Account.java
+++ Ch02/src/sub1/Account.java
@@ -5,22 +5,21 @@ import java.io.IOException;
 import javax.servlet.jsp.JspWriter;
 
 public class Account {
-	
+
 	private String bank;
 	private String id;
 	private String name;
 	private int    money;
 	
 	public Account(String bank, String id, String name, int money) {
-		this.bank  = bank;
-		this.id    = id;
-		this.name  = name;
+		this.bank = bank;
+		this.id = id;
+		this.name = name;
 		this.money = money;
 	}
 	
 	public void deposit(int money) {
 		this.money += money;
-		
 	}
 	
 	public void withdraw(int money) {
@@ -30,10 +29,10 @@ public class Account {
 	public void show(JspWriter out) throws IOException {
 		out.println("<h4>현재잔액</h4>");
 		out.println("<p>");
-		out.println("은 행 명 : "+bank+"br/");
-		out.println("계좌번호 : "+id+"br/");
-		out.println("입 금 주 : "+name+"br/");
-		out.println("현재잔액 : "+money+"br/");
+		out.println("은 행 명 : "+bank+"<br/>");		
+		out.println("계좌번호 : "+id+"<br/>");		
+		out.println("입 금 주 : "+name+"<br/>");		
+		out.println("현재잔액 : "+money+"<br/>");		
 		out.println("</p>");
 	}
 
@@ -68,7 +67,4 @@ public class Account {
 	public void setMoney(int money) {
 		this.money = money;
 	}
-	
-	
-
 }
