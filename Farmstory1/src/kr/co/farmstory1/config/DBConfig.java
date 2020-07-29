042b2655569ea96c4e3f6c3d329434925d31bb85
bigdata <bigdata@DESKTOP-OJ7SKR9>
2020-07-29 12:33:17 +0900

    Jboard1

---
+++ Farmstory1/src/kr/co/farmstory1/config/DBConfig.java
@@ -0,0 +1,28 @@
+package kr.co.farmstory1.config;
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class DBConfig {
+	
+	// 배포용
+	//private static final String HOST="jdbc:mysql://54.123.221.12:3306/jhr?/-----";
+	//private static final String USER="jhr";
+	//private static final String PASS="1234";
+	
+	// 개발용
+	private static final String HOST = "jdbc:mysql://192.168.50.82:3306/jhr";
+	private static final String USER = "jhr";
+	private static final String PASS = "1234";
+	
+	public static Connection getConnection() throws Exception {
+		
+		// 1단계
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		// 2단계
+		Connection conn = DriverManager.getConnection(HOST, USER, PASS);
+		
+		return conn;
+	}
+	
+
+}
